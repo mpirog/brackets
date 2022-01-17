4e39070ae8172606802bd7b7f1cf79d3cbcc717c
imychko <imychko@lumentouch.com>
2022-01-17 21:06:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+
+const getOpenBreket = (bracketsConfig, clBreket) => {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][1] === clBreket) {
+      return bracketsConfig[i][0]
+    }
+  }
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brekets = [...str]
+  const closeBrekets = bracketsConfig.map(element => element[1])
+
+  while (brekets.length) {
+    let currenIndex = 0,
+        sameBreket = null
+
+    const clBreket = brekets.find((breket, index) => {
+        currenIndex = index;
+
+        if (closeBrekets.includes(breket)) {
+          if (sameBreket !== breket && getOpenBreket(bracketsConfig, breket) === breket) {
+            sameBreket = breket
+          } else {
+            return true
+          }
+        }
+    })
+
+    let opBrecet = getOpenBreket(bracketsConfig, clBreket)
+  
+    if (!opBrecet || opBrecet !== brekets[currenIndex-1]) {
+      return false
+    }
+
+    brekets.splice(currenIndex-1, 2)
+  }
+
+  return true
 }
